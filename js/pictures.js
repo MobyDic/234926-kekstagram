69932d1f8dea5321a3939bf7eab7057caac1ba2d
Yaroslav Makarenko <mobydic@mail.ru>
2017-02-27 12:36:46 +0300

    Правка

--- js/pictures.js
+++ js/pictures.js
@@ -26,19 +26,20 @@
     // фильтр фото
     function selectFilter(e) {
       var element = e.target;
+      if (element.className !== 'filters') {
+        picturesContainer.innerHTML = '';
 
-      picturesContainer.innerHTML = '';
-
-      switch (element.value) {
-        case 'popular':
-          drawPictures(pictures);
-          break;
-        case 'new':
-          drawPictures(sortNew(pictures));
-          break;
-        case 'discussed':
-          drawPictures(sortDiscussed(pictures));
-          break;
+        switch (element.value) {
+          case 'popular':
+            drawPictures(pictures);
+            break;
+          case 'new':
+            drawPictures(sortNew(pictures));
+            break;
+          case 'discussed':
+            drawPictures(sortDiscussed(pictures));
+            break;
+        }
       }
     }
 
